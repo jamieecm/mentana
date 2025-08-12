d252cc45f06b6f3a99c04f271ca12f874751ddf1
Jaime <jcarrasco.datos@gmail.com>
2025-08-12 17:42:54 -0400

    commit directorios

---
+++ src/server/controllers/userController.js
@@ -0,0 +1,7 @@
+import { supabase } from '../services/supabaseClient.js';
+
+export const getUsers = async (req, res) => {
+  const { data, error } = await supabase.from('users').select('*');
+  if (error) return res.status(500).json({ error: error.message });
+  res.json(data);
+};
